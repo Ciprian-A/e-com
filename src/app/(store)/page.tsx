cad4c72561141ba61e41cc9489a9b006ba8b7851
21ciprian <2021ciprianoffice@gmail.com>
2024-11-23 01:14:24 +0000

    feat: configure products

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-	return <div>Hello Workd</div>
+import ProductsView from '@/components/ProductsView'
+import {getAllCategories} from '@/sanity/lib/products/getAllCategories'
+import {getAllProducts} from '@/sanity/lib/products/getAllProducts'
+
+export default async function Home() {
+	const products = await getAllProducts()
+	const categories = await getAllCategories()
+	return (
+		<div>
+			<h1>Hello Workd</h1>
+			<div className='flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4'>
+				<ProductsView categories={categories} products={products} />
+			</div>
+		</div>
+	)
 }
---
+++ src/components/ProductsView.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Category, Product} from '@/../sanity.types'
+import ProductGrid from './ProductGrid'
+interface ProductsViewProps {
+	products: Product[]
+	categories: Category[]
+}
+
+const ProductsView = ({products, categories}: ProductsViewProps) => {
+	return (
+		<div className='flex flex-col'>
+			{/*categories*/}
+			<div className='w-full sm:w-[200px]'>
+				{/* <CategorySelectorComponent categories={categories}  /> */}
+			</div>
+			{/*products*/}
+			<div className='flex-1'>
+				<div>
+					<ProductGrid products={products} />
+					<hr className='w-1/12 sm:w3/4' />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ProductsView
