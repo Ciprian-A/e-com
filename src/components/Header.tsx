af40e8c2b172286b7d8204324fd1b079d1d02c7d
21ciprian <2021ciprianoffice@gmail.com>
2024-11-24 19:51:59 +0000

    feat: show items count in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 'use client'
+import useBasketStore from '@/app/(store)/store'
 import {
 	ClerkLoaded,
 	SignedIn,
@@ -12,6 +13,9 @@ import Link from 'next/link'
 
 const Header = () => {
 	const {user} = useUser()
+	const itemCount = useBasketStore(state =>
+		state.items.reduce((total, item) => total + item.quantity, 0)
+	)
 
 	return (
 		<header className='flex flex-wrap justify-between items-center px-4 py-2'>
@@ -37,6 +41,9 @@ const Header = () => {
 					href='/basket'
 					className='flex flex-1 relative justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
 					<TrolleyIcon className='w-6 h-6' />
+					<span className='absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs'>
+						{itemCount}
+					</span>
 					<span>My Basket</span>
 				</Link>
 				<ClerkLoaded>
