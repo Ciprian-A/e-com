4960ea1f271a3a1224a27fc923264486b37b9b58
Ciprian-A <ciprianoffice10@gmail.com>
2024-12-19 23:52:05 +0000

    chore: change studio name

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -3,7 +3,7 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = S =>
 	S.list()
-		.title('Shopr E-comm')
+		.title('Prime-Shop E-Commerce')
 		.items([
 			S.documentTypeListItem('category').title('Categories'),
 			S.divider(),
