b8f0937c6b4d5b6dcf51a4d7f495037bb4077a1c
Ciprian-A <ciprianoffice10@gmail.com>
2024-12-20 10:30:06 +0000

    chore:change title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
 				<Link
 					href='/'
 					className='text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0'>
-					Shopr
+					Shoppy
 				</Link>
 				<Form
 					action='/search'
