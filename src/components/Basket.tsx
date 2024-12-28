bac68d76397e08b006eaea198fb82c70eb9c2e05
Ciprian-A <ciprianoffice10@gmail.com>
2024-12-28 12:56:08 +0000

    feat: add tooltip to basket component

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -2,22 +2,24 @@
 import Link from 'next/link'
 import {TrolleyIcon} from '@sanity/icons'
 import useBasketStore from '@/app/(store)/store'
+import TooltipHeader from './TooltipHeader'
 
 export const Basket = () => {
 	const itemCount = useBasketStore(state =>
 		state.items.reduce((total, item) => total + item.quantity, 0)
 	)
 	return (
-		<Link
-			href='/basket'
-			className='group flex flex-1 relative justify-center sm:justify-start sm:flex-none items-center space-x-2 hover:bg-gray-200 text-black font-bold py-1 px-2 rounded'>
-			<TrolleyIcon className='w-6 h-6' />
-			{itemCount > 0 && (
-				<span className='child absolute  -top-2 -right-2 bg-red-500 group-hover:bg-red-700 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs'>
-					{itemCount}
-				</span>
-			)}
-			<span className='hidden md:visible'>My Basket</span>
-		</Link>
+		<TooltipHeader description='My Basket'>
+			<Link
+				href='/basket'
+				className='group flex flex-1 relative justify-center sm:justify-start sm:flex-none items-center space-x-2 hover:bg-gray-200 text-black font-bold py-1 px-2 rounded'>
+				<TrolleyIcon className='w-6 h-6' />
+				{itemCount > 0 && (
+					<span className='child absolute  -top-2 -right-2 bg-red-500 group-hover:bg-red-700 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs'>
+						{itemCount}
+					</span>
+				)}
+			</Link>
+		</TooltipHeader>
 	)
 }
