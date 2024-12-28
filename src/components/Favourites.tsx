a475e665456130497f351c8b66fd4028d53ca173
Ciprian-A <ciprianoffice10@gmail.com>
2024-12-28 12:53:02 +0000

    feat: implement tooltip component

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -1,15 +1,16 @@
 import Link from 'next/link'
 import {HeartIcon} from '@sanity/icons'
+import TooltipHeader from './TooltipHeader'
 
 const Favourites = () => {
 	return (
-		<div>
+		<TooltipHeader description='Favourites'>
 			<Link
 				href='/favourites'
 				className='flex flex-1 relative justify-center sm:justify-start sm:flex-none items-center space-x-2 hover:bg-gray-200 text-black font-bold py-1 px-2 rounded'>
 				<HeartIcon className='w-6 h-6 ' />
 			</Link>
-		</div>
+		</TooltipHeader>
 	)
 }
 
---
+++ src/components/TooltipHeader.tsx
@@ -0,0 +1,25 @@
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from '@/components/ui/tooltip'
+
+interface TooltipHeaderProps {
+	children: React.ReactNode
+	description: string
+}
+const TooltipHeader = ({children, description}: TooltipHeaderProps) => {
+	return (
+		<TooltipProvider delayDuration={100}>
+			<Tooltip>
+				<TooltipTrigger asChild>{children}</TooltipTrigger>
+				<TooltipContent>
+					<p>{description}</p>
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	)
+}
+
+export default TooltipHeader
