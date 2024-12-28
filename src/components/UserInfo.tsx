c022256172480b9cb714c703a1584ef45e19e8b0
Ciprian-A <ciprianoffice10@gmail.com>
2024-12-28 12:59:24 +0000

    feat: add tooltip to orders

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -9,18 +9,20 @@ import {
 } from '@clerk/nextjs'
 import Link from 'next/link'
 import {UlistIcon} from '@sanity/icons'
+import TooltipHeader from './TooltipHeader'
 
 export const UserInfo = () => {
 	const {user} = useUser()
 	return (
 		<ClerkLoaded>
 			<SignedIn>
-				<Link
-					href='/orders'
-					className='flex flex-1 relative justify-center sm:justify-start sm:flex-none items-center space-x-2 hover:bg-gray-200 text-black font-bold py-1 px-2 rounded'>
-					<UlistIcon className='w-6 h-6' />
-					<span className='hidden md:visible'>Orders History</span>
-				</Link>
+				<TooltipHeader description='Orders'>
+					<Link
+						href='/orders'
+						className='flex flex-1 relative justify-center sm:justify-start sm:flex-none items-center space-x-2 hover:bg-gray-200 text-black font-bold py-1 px-2 rounded'>
+						<UlistIcon className='w-6 h-6' />
+					</Link>
+				</TooltipHeader>
 			</SignedIn>
 			{user ? (
 				<div className='flex items-center space-x-2'>
