c338d9831dc52b96b5b18a53f9183d45727ac851
Ciprian-A <ciprianoffice10@gmail.com>
2025-01-13 07:02:31 +0800

    change loading color

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
 	return (
 		<div className='flex justify-center itmes-center h-screen'>
-			<div className='animate-spin rounded-full h-32 w-32 border-b-2 border-blue-500' />
+			<div className='animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900' />
 		</div>
 	)
 }
