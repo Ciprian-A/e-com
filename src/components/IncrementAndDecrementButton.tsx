da30901048ab8da481314ab3b7b6a45195437b53
Ciprian-A <ciprianoffice10@gmail.com>
2025-01-13 06:58:28 +0800

    chore: cleanup

--- src/components/IncrementAndDecrementButton.tsx
+++ src/components/IncrementAndDecrementButton.tsx
@@ -1,21 +1,18 @@
 'use client'
 import React, {useEffect, useState} from 'react'
-// import {Footwear, Clothing} from '../../sanity.types'
 import useStore from '@/app/(store)/store'
 
 type IncrementAndDecrementButtonProps = {
 	productId: string
-	// product: (Clothing | Footwear) & {sizeId?: string}
 	disabled: boolean
 }
 
 const IncrementAndDecrementButton = ({
 	productId,
 	disabled
-	// itemSize
 }: IncrementAndDecrementButtonProps) => {
 	const {incrementItemCount, decrementItemCount, getItemCount} = useStore()
-	// const item = getItem(product, itemSize ?? '')
+
 	const itemCount = getItemCount(productId)
 	const [isClient, setIsClient] = useState(false)
 	useEffect(() => {
@@ -25,17 +22,11 @@ const IncrementAndDecrementButton = ({
 	if (!isClient) {
 		return null
 	}
-	// console.log({itemCount})
-	// console.log('AddToBasketButton>>>>>>>> item, product', {
-	// 	item,
-	// 	product,
-	// 	itemSize
-	// })
+
 	return (
 		<div className='flex items-center justify-center space-x-2'>
 			<button
 				onClick={() => decrementItemCount(productId)}
-				// onClick={() => decrementItemCount(product, itemSize ?? '')}
 				className={`w-8 h-8 rounded-full flex items-center justify-center transition-colors duration-200 ${itemCount === 0 ? 'bg-gray-100 cursor-not-allowed' : 'bg-gray-200 hover:bg-gray-300'}`}
 				disabled={itemCount === 0 || disabled}>
 				<span
@@ -46,7 +37,6 @@ const IncrementAndDecrementButton = ({
 			<span className='w-8 text-center font-semibold'>{itemCount}</span>
 			<button
 				onClick={() => incrementItemCount(productId)}
-				// onClick={() => incrementItemCount(product, itemSize ?? '')}
 				className={`w-8 h-8 rounded-full flex items-center justify-center transition-colors duration-200 ${disabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'}`}
 				disabled={disabled}>
 				<span className='text-xl font-bold text-white'>+</span>
