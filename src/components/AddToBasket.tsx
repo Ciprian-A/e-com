b3928cf6e50c264bebdb2a5c8812e1cb30f4826a
Ciprian-A <ciprianoffice10@gmail.com>
2025-01-13 14:58:15 +0800

    feat: disable actions on button when disabled

--- src/components/AddToBasket.tsx
+++ src/components/AddToBasket.tsx
@@ -10,11 +10,13 @@ interface AddToBasketProps {
 
 const AddToBasket = ({product, disabled}: AddToBasketProps) => {
 	const {addItemToBasket, getActiveSize, setActiveSize} = useStore()
-
 	const items = useStore().items
-	console.log({items, product})
+
 	const handleAddToBasket = (product: Footwear | Clothing) => {
 		const size = getActiveSize()
+		if (disabled) {
+			return
+		}
 		const itemToBeAddedToBasket = items.find(
 			i => i._id === `${product._id}-${size}`
 		)
@@ -27,8 +29,7 @@ const AddToBasket = ({product, disabled}: AddToBasketProps) => {
 
 	return (
 		<Button
-			disabled={disabled}
-			className={`${disabled ? 'cursor-not-allowed' : 'w-full rounded-md bg-gray-900 text-white text-base border-black border-2 hover:bg-gray-700 hover:scale-[1.01] hover:shadow-[0px_10px_15px_-5px_rgba(0,0,0,0.3)] py-5'}`}
+			className={`${disabled ? 'cursor-not-allowed' : ' disabled:bg-gray-900 w-full rounded-md bg-gray-900 text-white text-base border-black border-2 hover:bg-gray-700 hover:scale-[1.01] hover:shadow-[0px_10px_15px_-5px_rgba(0,0,0,0.3)] py-5'}`}
 			onClick={() => handleAddToBasket(product)}>
 			Add to basket
 		</Button>
