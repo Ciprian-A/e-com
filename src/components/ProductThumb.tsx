5801906140e55b556029efbd77ee5085e9fba13c
Ciprian-A <ciprianoffice10@gmail.com>
2025-01-17 23:31:23 +0800

    chore: cleanup

--- src/components/ProductThumb.tsx
+++ src/components/ProductThumb.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import {Clothing, Footwear} from '../../sanity.types'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import {imageUrl} from '@/lib/imageUrl'
