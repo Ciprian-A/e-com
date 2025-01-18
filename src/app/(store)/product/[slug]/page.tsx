19efc199636dc930461a0242c7e0317c7afb3648
Ciprian-A <ciprianoffice10@gmail.com>
2025-01-18 23:51:52 +0800

    chore: cleanup

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -24,7 +24,7 @@ const ProductPage = async ({params}: {params: Promise<{slug: string}>}) => {
 	if (!product) {
 		return notFound()
 	}
-	// const isOutOfStock = !product?.sizesAndStock?.some(p => (p?.stock ?? 0) > 0)
+
 	return (
 		<div className='container mx-auto px-4 py-8'>
 			<div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
