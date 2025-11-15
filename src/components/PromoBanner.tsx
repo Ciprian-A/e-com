188d6c7b945b02b0cc649dbf580185dc43dffb5b
Ciprian-A <ciprianoffice10@gmail.com>
2025-11-15 00:25:06 +0000

    update banner

--- src/components/PromoBanner.tsx
+++ src/components/PromoBanner.tsx
@@ -1,4 +1,4 @@
-import {getActiveSaleByCuponCode} from '@/sanity/lib/sales/getActiveSaleByCuponCode'
+import { getActiveSaleByCuponCode } from '@/sanity/lib/sales/getActiveSaleByCuponCode'
 
 const PromoBanner = async () => {
 	const sale = await getActiveSaleByCuponCode('PROMO25')
@@ -7,17 +7,17 @@ const PromoBanner = async () => {
 	}
 
 	return (
-		<div className='bg-gradient-to-r from-red-600 to-black text-white ps-6 py-10 mx-4 mt-2 rounded-lg shadow-lg'>
-			<div className='container mx-auto flex items-center justify-between'>
+		<div className='bg-linear-to-r from-[#115acf] via-purple-500 to-[#115acf] text-white ps-6 py-10 mx-4 mt-2 rounded-lg shadow-lg'>
+			<div className='container mx-auto flex items-center '>
 				<div className='flex-1'>
-					<h2 className='text-3xl sm:text-5xl font-extrabold text-left mb-4'>
+					<h2 className='text-3xl sm:text-5xl font-extrabold text-center mb-4'>
 						{sale.tilte}
 					</h2>
-					<p className='text-left text-xl sm:text-3xl font-semibold-mb-6'>
+					<p className=' text-center text-xl sm:text-3xl font-semibold mb-6'>
 						{sale.description}
 					</p>
-					<div className='flex'>
-						<div className='bg-white text-black py-4 px-6 rounded-full shadow-md transform hover:scale-105 transition duration-300'>
+					<div className='flex justify-center'>
+						<div className='bg-white text-black py-4 px-6 rounded-full shadow-md'>
 							<span className='font-bold text-base sm:text-xl'>Use code: </span>
 							<span className='font-bold text-red-600'>{sale.couponCode}</span>
 							<span className='ml-2 font-bold text-base sm:text-xl'>
