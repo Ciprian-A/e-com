b7919804d8d83ceab2950f9f72fe32a41e230df5
Ciprian-A <ciprianoffice10@gmail.com>
2026-01-15 17:27:12 +0000

    replace sanity on home page

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -2,25 +2,27 @@ import ProductsView from '@/components/ProductsView'
 import PromoBanner from '@/components/PromoBanner'
 import { getCategories } from '@/lib/categories/categories'
 import { getItems } from '@/lib/items/items'
-import { getAllCategories } from '@/sanity/lib/products/getAllCategories'
-import { getAllProducts } from '@/sanity/lib/products/getAllProducts'
 
 export const dynamic = 'force-static'
 export const revalidate = 60 // revaliate at most every 60 seconds
 
 export default async function Home() {
 	const items = await getItems()
-	const categs = await getCategories()
-	const products = await getAllProducts()
-	const categories = await getAllCategories()
+	const categories = await getCategories()
+	const mappedProducts = items.map(product => ({
+		...product,
+		productDetails: Array.isArray(product.productDetails)
+			? (product.productDetails as {key: string; value: string}[])
+			: []
+	}))
 	console.log(
-		`${crypto.randomUUID().slice(0, 5)} >>> Rendered the home page chache with ${products.length} products and ${categories.length} categories`
+		`${crypto.randomUUID().slice(0, 5)} >>> Rendered the home page chache with ${items.length} products and ${categories.length} categories`
 	)
 	return (
 		<div>
 			<PromoBanner />
 			<div className='flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4'>
-				<ProductsView products={items} />
+				<ProductsView products={mappedProducts} />
 			</div>
 		</div>
 	)
