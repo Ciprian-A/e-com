2c5cf61ba1225c493c3d0fab15c4a4f33fcf0922
Ciprian-A <ciprianoffice10@gmail.com>
2026-01-23 03:41:44 +0000

    fix build

--- src/app/(store)/admin/categories/[categoryId]/edit/page.tsx
+++ src/app/(store)/admin/categories/[categoryId]/edit/page.tsx
@@ -1,12 +1,13 @@
 import EditCategoryForm from '@/components/categories/EditCategoryForm'
-import { getCategory } from '@/lib/categories/categories'
+import {getCategory} from '@/lib/categories/categories'
 
 async function Category({params}: {params: Promise<{categoryId: string}>}) {
 	const {categoryId} = await params
 	const categoryRaw = await getCategory(categoryId)
 	const category = {
 		...categoryRaw,
-		description: categoryRaw?.description ?? undefined
+		description: categoryRaw?.description ?? undefined,
+		categoryImageUrl: categoryRaw?.categoryImageUrl ?? undefined
 	}
 	return (
 		<div>
