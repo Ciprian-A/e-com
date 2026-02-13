f5d3b455e5fcd968a0a46ba68ddd33003104a43e
Ciprian-A <ciprianoffice10@gmail.com>
2026-02-13 22:27:09 +0800

    fix prisma pooling

--- config/db.ts
+++ config/db.ts
@@ -1,13 +1,19 @@
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Prisma, PrismaClient } from '../generated/prisma/client'
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL
-})
-const prisma = new PrismaClient({
-	log: ['query', 'info', 'warn', 'error'],adapter
-})
+import {PrismaPg} from '@prisma/adapter-pg'
+import {Prisma, PrismaClient} from '../generated/prisma/client'
 
-const globalForPrisma = global as unknown as {prisma: typeof prisma}
+const globalForPrisma = global as unknown as {prisma: PrismaClient}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-export { prisma, Prisma }
+const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		log: ['query', 'info', 'warn', 'error'],
+		adapter: new PrismaPg({
+			connectionString: process.env.DATABASE_URL
+		})
+	})
+
+if (process.env.NODE_ENV !== 'production') {
+	globalForPrisma.prisma = prisma
+}
+
+export {prisma, Prisma}
