d68ea15b7caeb51d57d32ab701f802553a5d0c7c
Ciprian-A <ciprianoffice10@gmail.com>
2026-03-06 18:25:30 +0000

    hide buy box if out of stock

--- src/components/BuyBox.tsx
+++ src/components/BuyBox.tsx
@@ -37,46 +37,53 @@ const BuyBox = ({
 
 	return (
 		<div className='w-full lg:w-[35%] flex flex-col border rounded-md p-3 mt-4 lg:mt-0 max-h-max'>
-			<div className='h-full flex flex-col justify-between'>
+			<div className='h-auto flex flex-col justify-between'>
 				<div>
 					<p>£{price}</p>
 					<p
 						className={`text-xl font-semibold ${isOutOfStock ? 'text-red-500' : 'text-green-600'}`}>
 						{isOutOfStock ? 'Out of stock' : 'In stock'}
 					</p>
-					{!isOutOfStock && (
+					{isOutOfStock ? (
+						<p className='text-sm text-gray-500 mt-2'>
+							The item will be back in stock in a few days.
+						</p>
+					) : (
 						<p className='text-sm text-gray-500 mt-2'>
 							The delivery is estimated to take 3-5 working days.
 						</p>
 					)}
 				</div>
-				<Separator className='my-4' />
-				<div className=' space-y-3'>
-					<div>
-						<p className='text-base mb-2'>Quantity: {quantity}</p>
-						<QuantitySelector qty={availableStock ?? 0} productId={id} />
-					</div>
-					<BuyItNow
-						productId={id}
-						name={name}
-						slug={slug}
-						price={price}
-						image={image}
-						variants={variants}
-						productType={product.type}
-						availableStock={availableStock ?? 0}
-					/>
-					<AddToBasket
-						productId={id}
-						name={name}
-						slug={slug}
-						price={price}
-						image={image}
-						// variants={variants}
-						productType={product.type}
-						availableStock={availableStock ?? 0}
-					/>
-				</div>
+				{!isOutOfStock && (
+					<>
+						<Separator className='my-4' />
+						<div className=' space-y-3'>
+							<div>
+								<p className='text-base mb-2'>Quantity: {quantity}</p>
+								<QuantitySelector qty={availableStock ?? 0} productId={id} />
+							</div>
+							<BuyItNow
+								productId={id}
+								name={name}
+								slug={slug}
+								price={price}
+								image={image}
+								variants={variants}
+								productType={product.type}
+								availableStock={availableStock ?? 0}
+							/>
+							<AddToBasket
+								productId={id}
+								name={name}
+								slug={slug}
+								price={price}
+								image={image}
+								productType={product.type}
+								availableStock={availableStock ?? 0}
+							/>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	)
